86693dafcaf43fa39b6935ae93e6c2d302f4505e
Renan Perez <perez.vr@gmail.com>
2020-04-18 01:05:58 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./App.css";
+
+import profile from './assets/profile.jpg';
+
+function App(){
+  return <img width="200" src={profile}/>;
+}
+
+export default App;
